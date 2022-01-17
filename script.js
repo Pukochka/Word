974ae200caf2b029fdfcf1acac7313415155f4e3
pukochka <tema.puko@gmail.com>
2022-01-18 02:11:03 +0300

    new

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
     const btn = {
-        weight : document.querySelector('#weight'),
-        italics : document.querySelector('#italics'),
-        underline : document.querySelector('#underline'),
+        bold : document.querySelector('#bold'),
+        italic : document.querySelector('#italic'),
+        line : document.querySelector('#line'),
         link : document.querySelector('#link'),
-        redact : document.querySelector('#redact'),
-        unred : document.querySelector('#addRed')
+        code : document.querySelector('#code'),
+        sp : document.querySelector('#sp')
     }
     let text = {
-        entr : document.querySelector('#textarea'),
-        oiu : document.querySelector('#out'),
-        out : document.querySelector('#in')
+        outCode : document.querySelector('#textarea'),
+        editor : document.querySelector('#editor')
     }
     const tags = {
         istart : '<i>',
@@ -18,96 +17,50 @@
         bend : '</b>',
         astart : '<a href="#">',
         aend : '</a>',
-        ustart : '<span class="underline">',
-        uend : '</span>',
+        lstart : '<span class="line">',
+        lend : '</span>',
+    }
+
+    let content,
+        selection,
+        start,
+        end;
+
+        let i =0;
+    
+    
+    document.onselectionchange = function() {
+        selection = document.getSelection();
+        
+        
+        
+    };
+    function setStyle(start,end){
+        if(text.editor.innerHTML == '' ){return}
+        let { startOffset , endOffset} = selection.getRangeAt(i);
+        console.log(startOffset,endOffset)
+        let inner = [...text.editor.innerHTML];
+        inner.splice(startOffset,0,start);
+        inner.splice(endOffset + 1  ,0,end);
+        
+        let total = inner.join('');
+        
+        text.editor.innerHTML = total;
+        selection.removeAllRanges()
     }
     
-    let content,
-        remake,
-        symbolindex,
-        symbolLenght,
-        insert,
-        total,
-        arr;
-        
-    function validate(){
-        if(text.entr.value == ''){   
-            btn.weight.disabled = true;
-            btn.italics.disabled = true;   
-            btn.underline.disabled = true;   
-            btn.link.disabled = true;   
-        }
-    }
-
-    text.entr.addEventListener('keyup',function(e){
-        text.out.innerHTML = text.entr.value;
-    });
-
-    function synchronTx(){
-        text.out.innerHTML
-    }
-
-    function selectedTx(e){
-        content = window.getSelection().toString();
-        
-        remake = text.out.innerHTML.match(`${content}`);
-        
-        symbolindex = remake['index'];
-        symbolLenght = remake[0].length;
-
-        insert = symbolindex + symbolLenght + 1;
-        total = [...text.out.innerHTML];
-
-        console.log(e);
-    }
-
-    function edit(start,end){
-
-        total?.splice(symbolindex,0,start);
-        total?.splice(insert,0,end);
-
-        arr = total?.join('');
-
-        text.out.innerHTML = arr;
-    }
-
-    btn.redact.addEventListener('click',function(){
-        document.querySelector('.buttons_main').classList.add('hidden');
-        document.querySelector('.buttons_redaction').classList.add('active');
-
-        text.entr.classList.add('tehidden');
-        text.oiu.classList.add('active')
-    });
-
-    btn.unred.addEventListener('click',function(){
-        document.querySelector('.buttons_main').classList.remove('hidden');
-        document.querySelector('.buttons_redaction').classList.remove('active');
-
-        text.entr.classList.remove('tehidden');
-        text.oiu.classList.remove('active')
-    });
-
-    btn.italics.addEventListener('click',function(){
-        
-        edit(tags.istart,tags.iend)
-    });
-
-    btn.weight.addEventListener('click',function(){
-        
-        edit(tags.bstart,tags.bend)
-    });
-
+    btn.bold.addEventListener('click',function(){
+        setStyle(tags.bstart,tags.bend)
+    }) ;
     btn.link.addEventListener('click',function(){
-        
-        edit(tags.astart,tags.aend)
-    });
-
-    btn.underline.addEventListener('click',function(){
-        
-        edit(tags.ustart,tags.uend)
-    });
-
-    text.oiu.addEventListener('mouseup',selectedTx);
-
-    text.oiu.addEventListener('touchend',selectedTx);
-    text.oiu.addEventListener('touch',selectedTx);
+        setStyle(tags.astart,tags.aend)
+    }) ;
+//     btn.sp.addEventListener('click',function(){
+//         setStyle(tags.bstart,tags.bend)
+//    }) ;
+    btn.line.addEventListener('click',function(){
+        setStyle(tags.lstart,tags.lend)
+    }) ;
+    btn.italic.addEventListener('click',function(){
+        setStyle(tags.istart,tags.iend)
+    }) ;
