588a0d5a35a3622325ca40c3411673c422650a49
pukochka <tema.puko@gmail.com>
2022-01-22 01:50:01 +0300

    add tag P, undo code

--- script.js
+++ script.js
@@ -3,33 +3,55 @@
     const buttons = {
         link : document.querySelector('#link'),
         sp : document.querySelector('#sp'),
-        code : document.querySelector('#code')
+        code : document.querySelector('#code'),
+        delete : document.querySelector('#delete'),
+        undo : document.querySelector('#undo')
     }
 
-    let select;
+    let select,
+        outH,
+        editor = rich.document,
+        inner;
     
     function designMode(){
-        rich.document.designMode = 'On';
-        rich.document.body.style.wordWrap = 'break-word';
-        rich.document.body.style.fontFamily = 'sans-serif';
-        rich.document.body.setAttribute('spellcheck','false');
-        rich.document.body.setAttribute('contenteditable','true');
+        editor.designMode = 'On';
+        editor.body.style.wordWrap = 'break-word';
+        // editor.body.style.fontFamily = 'sans-serif';
+        editor.body.setAttribute('spellcheck','false');
+        editor.body.setAttribute('contenteditable','true');
+        editor.execCommand('defaultParagraphSeparator', false, "p");
     }
 
     function setStyle(style){
-        rich.document.execCommand(style);
+        editor.execCommand(style);
     }
 
-        rich.document.onselectionchange = function(){
-            select = rich.document.getSelection();
+    function editP(){
+        let i = 0;
+        editor.body.addEventListener('focus',function(){
+            if(i > 0){return}
+
+                let pi = document.createElement('p');
+                pi.innerHTML = '&nbsp;';
+                this.append(pi);
+
+                i++;
+        });
+    }
+    
+    editP();
+        
+        editor.onselectionchange = function(){
+            select = editor.getSelection();
+            console.log(select)
         }
 
-        for(let btn of simpleButtons){
-            btn.addEventListener('click',function(){
-                let data = btn.getAttribute('data-style');
-                setStyle(data);
-            });
-        }
+            for(let btn of simpleButtons){
+                btn.addEventListener('click',function(){
+                    let data = btn.getAttribute('data-style');
+                    setStyle(data);
+                });
+            }
 
         const createLinkActive = () => {
             document.querySelector('.createlink').classList.add('active');
@@ -41,7 +63,13 @@
             document.querySelector('.editor_input').classList.remove('hidden');
         };
 
-        const visibleCode = () => document.querySelector('.codearea').classList.toggle('active')
+        const visibleCode = () => document.querySelector('.codearea').classList.toggle('active');
+
+        let codeValue = () => {
+            outH = editor.body.innerHTML;
+            let n = outH.replace(/&nbsp;/gi, '');
+            document.querySelector('.codearea').value = n;
+        }
 
         buttons.link.addEventListener('click',function(e){
             createLinkActive();
@@ -49,28 +77,30 @@
                 switch(e.keyCode) {
                     case 13 :
                         let joinLink = document.querySelector('.createlink').value;
-                        rich.document.execCommand('createLink',false,joinLink);
+                        editor.execCommand('createLink',false,joinLink);
+
                         createLinkHidden();
                         break;
                 }
             });
         });
 
-        buttons.code.addEventListener('click',function(e){
-
-            let outH = rich.document.body.innerHTML;
-            document.querySelector('.codearea').textContent =`<p>${outH}</p>`;
-
-            document.addEventListener('keydown',function(){
-                rich.document.body.innerHTML = document.querySelector('.codearea').value;
-            })
+        buttons.code.addEventListener('click',function(){
+            
+            codeValue();
+        
+            document.addEventListener('keydown',() => {editor.body.innerHTML = document.querySelector('.codearea').value})
+            
             visibleCode();
         });
 
         buttons.sp.addEventListener('click',function(){
-            let i = 0 ;
-            let content = select.getRangeAt(i).toString();
+            let i = 0, content = select.getRangeAt(i).toString();
             
-            rich.document.execCommand('insertHTML',false,`<span class="tg-spoiler">${content}</span>`)
+            editor.execCommand('insertHTML',false,`<span class="tg-spoiler">${content}</span>`)
         });
-        
+
+        buttons.delete.addEventListener('click',codeValue);
+
+        buttons.undo.addEventListener('click',codeValue);
+    
